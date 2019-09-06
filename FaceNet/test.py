4ce91cea89b4c6626c2bf7b6f0d1f50a9c865e94
Surya Rajendhran <cielograndesurya@gmail.com>
2019-09-06 19:29:06 +0530

    remove test file

--- FaceNet/test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-ag = argparse.ArgumentParser()
-ag.add_argument('-i', '--dataset', help='path to input directory of images')
-args = ag.parse_args()
-print(args)
-if args.dataset:
-	print('True')
