6fa9cbb7298d8c0f0c5b1a317cc704f650c89a31
Surya Rajendhran <cielograndesurya@gmail.com>
2019-09-06 17:30:14 +0530

    Change code to comply with PEP8

--- OpenCV/train_model.py
+++ OpenCV/train_model.py
@@ -7,11 +7,11 @@ import pickle
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--embeddings", required=True,
-	help="path to serialized db of facial embeddings")
+                help="path to serialized db of facial embeddings")
 ap.add_argument("-r", "--recognizer", required=True,
-	help="path to output model trained to recognize faces")
+                help="path to output model trained to recognize faces")
 ap.add_argument("-l", "--le", required=True,
-	help="path to output label encoder")
+                help="path to output label encoder")
 args = vars(ap.parse_args())
 
 # load the face embeddings
